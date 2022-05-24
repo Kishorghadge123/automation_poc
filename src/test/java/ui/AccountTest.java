dae5bdb1b465cb07332e6042c16cef3fe121cf52
LAPTOP-T841BI7R\vishal <megha.rathod@neutrinotechlabs.com>
2022-05-24 21:11:03 +0530

    Added Login Test Account Test And Put Request

--- src/test/java/ui/AccountTest.java
+++ src/test/java/ui/AccountTest.java
@@ -3,6 +3,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -10,26 +11,27 @@ import org.testng.annotations.Test;
 import pages.AccountPage;
 import pages.HomePage;
 import utilities.DriverInit;
+import utilities.Functions;
 
 import java.io.IOException;
 import java.util.Properties;
 
-public class AccoutTest extends DriverInit
-{
-    static Properties testdata;
-    @Parameters("environment")
+public class AccountTest extends DriverInit{
+public static WebDriver driver;
+    public static Properties test_data;
     @BeforeClass(groups="ui")
+    @Parameters("environment")
     public void testStartUp(String env) throws IOException {
-        testdata = utilities.InitTestData.getTestData(env);
+        test_data = utilities.InitTestData.getTestData(env);
         driver = DriverInit.driver;
     }
 
     @Severity(SeverityLevel.CRITICAL)
     @Story("story_id: 003 -  Account showcase test")
-    @Description("verify user able to user able to created Account ")
+    @Description("verify user able to create Account ")
     @Test(priority='3',groups="smoke")
-    public void AccountTest() throws InterruptedException {
-
+    public void successful_Account_Test() throws InterruptedException {
+        Functions.openURL(test_data.getProperty("url"));
         AccountPage accountPage=new AccountPage();
         accountPage.clickAccount();
         accountPage.clickShowCart();
@@ -44,12 +46,9 @@ public class AccoutTest extends DriverInit
     @Story("story_id: 004 - logout")
     @Description("verify user able to logout ")
     @Test(priority='4',groups="smoke")
-    public void logout(){
-        HomePage homePage=new HomePage();
-        homePage.clikonlogoutbutton();
-    }
-    @AfterTest
-    public void closeBrowser() {
-        driver.quit();
+    public void logoutButton() {
+        HomePage homePage = new HomePage();
+        homePage.clickonlogoutbutton();
+
     }
 }
--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 
 import io.qameta.allure.*;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
+//import org.testng.Assert;
 import org.testng.annotations.*;
 
 import pages.LoginPage;
@@ -16,53 +16,66 @@ import utilities.Functions;
 @Feature("Login feature")
 @Listeners(utilities.ListenerUtils.class)
 public class LoginTest extends DriverInit {
-	
+
 	public static WebDriver driver;
-	public static Properties testdata;
+	public static Properties test_data;
 	
-	@Parameters("environment")
 	@BeforeClass (groups="ui")
+	@Parameters("environment")
 	public void testStartUp(String env) throws IOException {
-		testdata = utilities.InitTestData.getTestData(env);
+		System.out.println(env);
+		test_data = utilities.InitTestData.getTestData(env);
 		driver = DriverInit.driver;
 	}
-
-	@AfterTest
-	public void closeBrowser(){
-		driver.quit();
-	}
-
 	@Severity(SeverityLevel.CRITICAL)
 	@Story("story_id: 001 - successful login test")
 	@Description("verify user able to login to OpenKart app with valid credentials")
 	@Test (priority=1, groups="smoke", description = "verify successful login test")
 	public void successful_Login_Test() throws InterruptedException {
-		Functions.openURL(testdata.getProperty("url"));
+		System.out.println("Inside Login");
+		System.out.println(test_data.getProperty("url"));
+		Functions.openURL(test_data.getProperty("url"));
 		LoginPage loginPage = new LoginPage();
+		System.out.println("Before Login");
 		loginPage.goToLogin();
-		loginPage.enterUsername(testdata.getProperty("username"));
-		loginPage.enterPassword(testdata.getProperty("password"));
+		System.out.println("After Login");
+		loginPage.enterUsername(test_data.getProperty("username"));
+		loginPage.enterPassword(test_data.getProperty("password"));
+		System.out.println("Before button click");
 		loginPage.clickLoginButton();
+		loginPage.enterpin("4567");
+		loginPage.clickOnContinue();
+		System.out.println("after button click");
 		HomePage homePage = new HomePage();
-		Functions.waitForElementLoad(HomePage.welcomeText,testdata.getProperty("default_timeout"));
-		Functions.verifyElementDisplayed(HomePage.welcomeText);
-	}
 
+		Functions.waitForElementLoad(HomePage.welcomeText, test_data.getProperty("default_timeout"));
+		homePage.welcomeOpenCart();
+		Functions.verifyElementDisplayed(HomePage.welcomeText);
+		homePage.clickonlogoutbutton();
+
+	}
 	@Severity(SeverityLevel.NORMAL)
 	@Story("story_id: 002 - invalid login test")
 	@Description("verify user not able to login to OpenKart app with invalid credentials")
 	@Test (priority=2, groups="smoke", description = "verify unsuccessful login test")
 	public void invalid_Login_Test() throws InterruptedException {
-		driver.get(testdata.getProperty("url"));
+
+		driver.get(test_data.getProperty("url"));
+
+
 		LoginPage loginPage = new LoginPage();
 		loginPage.goToLogin();
-		loginPage.enterUsername(testdata.getProperty("username"));
+		loginPage.enterUsername(test_data.getProperty("username"));
 		loginPage.enterPassword("invalid");
 		loginPage.clickLoginButton();
-
-		Functions.waitForElementLoad(loginPage.errorMessage, testdata.getProperty("default_timeout"));
-		Functions.verifyElementDisplayed(loginPage.errorMessage);
+		Functions.waitForElementLoad(LoginPage.errorMessage, test_data.getProperty("default_timeout"));
+		Functions.verifyElementDisplayed(LoginPage.errorMessage);
 	}
-}
+	@AfterTest
+	public void closeBrowser(){
+		System.out.println("Before quit");
+		driver.quit();
+		System.out.println("After quit");
+	}
 
-
+			}
