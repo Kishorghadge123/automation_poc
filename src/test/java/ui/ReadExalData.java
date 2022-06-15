c9a897a8b190d17f834b37cf67eb3dab74781a19
NTS-AmitKorade <amitark1995@gmail.com>
2022-06-15 14:01:32 +0530

    commit 1

--- src/test/java/ui/ReadExalData.java
+++ src/test/java/ui/ReadExalData.java
@@ -5,14 +5,17 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.tika.exception.EncryptedDocumentException;
+import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
+@Test
 public class ReadExalData {
     public void m1 () throws EncryptedDocumentException, IOException {
         String path="D:\\ExalSheet\\Book1.xlsx";
+
         FileInputStream inputstream=new FileInputStream(path);
 
         XSSFWorkbook Workbook=new XSSFWorkbook(inputstream);
@@ -21,7 +24,7 @@ public class ReadExalData {
         Iterator iterator=sheet.iterator();
         while(iterator.hasNext())
         {
-            XSSFRow row =(XSSFRow) iterator.next();
+            XSSFRow row =(XSSFRow)iterator.next();
             Iterator cellIterator=row.cellIterator();
 
             while(cellIterator.hasNext())
